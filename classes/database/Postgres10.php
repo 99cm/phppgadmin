6b0931f68cf5f9a7080746fbfed2d0722924592b
Leo Wang <zyizyao@china-guide.com>
2018-11-07 23:41:19 -0500

    Update Postgres10.php

--- classes/database/Postgres10.php
+++ classes/database/Postgres10.php
@@ -18,13 +18,5 @@ class Postgres10 extends Postgres11 {
 	function __construct($conn) {
 		parent::__construct($conn);
 	}
-
-	// Help functions
-
-	function getHelpPages() {
-		include_once('./help/PostgresDoc10.php');
-		return $this->help_page;
-	}
-
 }
 ?>
